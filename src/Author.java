37267316b545331848c0ee6dae371b68c5267f3d
Viktor <viktor.ihtiyarov@gmail.com>
2022-08-05 10:39:23 +0700

    homework

---
+++ src/Author.java
@@ -0,0 +1,10 @@
+public class Author {
+    String firstName;
+    String secondName;
+
+    public Author(String firstName,String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+}
---
+++ src/Book.java
@@ -0,0 +1,10 @@
+public class Book {
+    String title;
+    int year;
+
+    public Book (String title, Author author, int year) {
+        this.title = title;
+        this.year = year;
+    }
+
+}

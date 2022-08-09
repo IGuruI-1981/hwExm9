6ea10d6632cf0ac7db5c721d05e2481b55d41cf7
Viktor <viktor.ihtiyarov@gmail.com>
2022-08-10 00:01:23 +0700

    homework

--- src/Author.java
+++ src/Author.java
@@ -1,10 +1,23 @@
 public class Author {
-    String firstName;
-    String secondName;
+    private String firstName;
+    private String secondName;
 
     public Author(String firstName,String secondName) {
         this.firstName = firstName;
         this.secondName = secondName;
+    }
+
+    public String getFirstName() {
+
+        return this.firstName;
+    }
+
+    public String getSecondName() {
+        return this.secondName;
+    }
+
+    public String toString() {
+        return "Автор: " + this.firstName + " " + this.secondName;
     }
 
 }
--- src/Book.java
+++ src/Book.java
@@ -1,10 +1,28 @@
 public class Book {
-    String title;
-    int year;
+    private String title;
+    private int year;
+    private String author;
 
-    public Book (String title, Author author, int year) {
+
+    public Book (String title, String author, int year) {
         this.title = title;
+        this.author = author;
         this.year = year;
     }
 
+    public String getTitle() {
+        return this.title;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public void setYear(int year) {
+         this.year = year;
+    }
+
+   public String getAuthor() {
+        return author;
+    }
 }
